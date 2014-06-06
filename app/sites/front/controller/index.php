8d1371884cf16d222266ee4c69776debabafec3c
hikaMaeng <hika00@gmail.com>
2014-06-06 22:12:46 +0900

    file테스트 통합

--- app/sites/front/controller/index.php
+++ app/sites/front/controller/index.php
@@ -8,33 +8,38 @@ class Controller{
 	private function assert( $v0, $v1 ){
 		return '<div style="color:#'.( $v0 === $v1 ? '090">OK' : '900">NO' ).'</div>';
 	}
+	private function subTitle($v){
+		bs::out( '<h2>'.$v.'</h2>' );
+	}
 	private function back(){
 		bs::out( '<br><div><a href="'.$this->url.'">Back</a></div>' );
 	}
 	
 	public function index(){
 		bs::out( 'TestList<br><br>',
-			'<div><a href="'.$this->url.'/fileGet">fileGet</a></div>',
-			'<div><a href="'.$this->url.'/fileSet">fileSet</a></div>'
+			'<div><a href="'.$this->url.'/file">file</a></div>'
 		);
 	}
-	public function fileGet(){
+	public function file(){
+		$this->subTitle('파일읽기');
 		$v0 = bs::file('test/testGet.txt');
 		$v1 = bs::file('/test/testGet.txt');
 		bs::out(
 			'test/testGet.txt : '.$v0, $this->assert( $v0, '안녕!' ), 
 			'/test/testGet.txt : '.$v1, $this->assert( $v1, '안녕!' )
 		);
-		$this->back();
-	}
-	public function fileSet(){
+		
+		$this->subTitle('파일삭제');
 		bs::file( 'test/testSet.txt', null );
 		$v0 = bs::file( 'test/testSet.txt' );
-		bs::out( '지우기', $this->assert( $v0, FALSE ) );
+		bs::out( 'test/testSet.txt : '.$v0, $this->assert( $v0, FALSE ) );
+		
+		$this->subTitle('파일쓰기');
 		$contents = '안녕쓰기!';
 		bs::file( '/test/testSet.txt', $contents );
 		$v0 = bs::file( 'test/testSet.txt' );
 		bs::out( $v0.':'.strlen($v0).':'.strlen('안녕쓰기!'), $this->assert( $v0, $contents ) );
+		
 		$this->back();
 	}
 }
