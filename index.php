515e39dffafe73bfdc5f9c0155aaeae46bfe2392
kota <kota@fivethirty.kr>
2014-07-18 10:25:25 +0900

    쉘모드에서 상대경로로 스크립트 허용하게 변경

--- index.php
+++ index.php
@@ -3,10 +3,13 @@ define( 'ID', 'front' );
 
 if( defined('STDIN') || ( empty($_SERVER['REMOTE_ADDR']) and !isset($_SERVER['HTTP_USER_AGENT'] ) and count($_SERVER['argv']) > 0) ) {
 	$t0 = explode( '/', $_SERVER['argv'][0] );
-	array_shift($t0);
+	if( substr( $_SERVER['argv'][0], 0, 1 ) == '/' ) array_shift($t0);
 	array_pop($t0);
+	$t0 = implode( "/", $t0 );
 
-	define( 'ROOT', '/'.implode("/", $t0).'/' );
+	if( strpos(  $_SERVER['argv'][0], realpath('') ) !== FALSE ) define( 'ROOT', '/'.$t0.'/' );
+	else define( 'ROOT', realpath('').'/'.$t0.'/' );
+
 	define( 'SHELL_MODE', TRUE );
 } else {
 	define( 'ROOT', realpath('').'/' );
